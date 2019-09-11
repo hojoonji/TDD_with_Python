a803bf296442f680f822e698efdc3e470ebd6103
hj <hj@toss.im>
2019-09-11 17:49:42 +0900

    POST 후에 redirect 하고 화면에 모든 항목을 표시해준다.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -13,8 +13,27 @@ class HomePageTest(TestCase):
 
     def test_home_page_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "신규 작업 아이템")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_save_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='신규 작업 아이템 1')
+        Item.objects.create(text='신규 작업 아이템 2')
+
+        response = self.client.get('/')
+        self.assertIn('신규 작업 아이템 1', response.content.decode())
+        self.assertIn('신규 작업 아이템 2', response.content.decode())
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
