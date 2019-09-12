d66614e3483628d240e8573b699e276dd0a38da7
hj <hj@toss.im>
2019-09-12 23:40:07 +0900

    * home.html 단순화 * list.html이 목록을 표시함. home.html은 입력만 받는 역할 * POST 후에 redirection을 특정 url로 함 * 기존의 home_page 뷰는 입력만 받는 역할을 하고 view_list는 화면에 데이터를 뿌리는 역할을 전담

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다.
         self.browser.get(self.live_server_url)
@@ -38,7 +38,7 @@ class NewVisitorTest(LiveServerTestCase):
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
         self.assertIn("To-Do", self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('작업 목록 시작', header_text)
 
         # 그녀는 바로 작업을 추가하기로 한다.
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -52,11 +52,6 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: 공작깃털 사기')
 
-        # 그녀는 그녀의 리스트에 새로운 URL이 할당된 것을 확인했다.
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
-
-
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다.
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -66,6 +61,20 @@ class NewVisitorTest(LiveServerTestCase):
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
         self.wait_for_row_in_list_table('1: 공작깃털 사기')
         self.wait_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+
+        # 그녀는 만족한 채로 잠 자러 간다.
+
+    def test_multiple_uesrs_can_start_lists_at_different_urls(self):
+        # 에디스는 새로운 작업 목록을 만든다.
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('공작깃털 사기')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: 공작깃털 사기')
+
+        # 그녀는 그녀의 리스트에 새로운 URL이 할당된 것을 확인했다.
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
         # 새로운 사용자인 프란시스가 사이트에 접속하다.
 
@@ -77,7 +86,7 @@ class NewVisitorTest(LiveServerTestCase):
         # 프란시스가 홈페이지에 접속한다.
         # 에디스의 리스트는 보이지 않는다.
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body')
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertNotIn('공작길털을 이용해서 그물 만들기', page_text)
 
@@ -85,6 +94,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('우유 사기')
         inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: 우유 사기')
 
         # 프란시스가 전용 URL을 취득한다.
         francis_url = self.browser.current_url
@@ -92,7 +102,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(edith_list_url, francis_url)
 
         # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다.
-        page_text = self.browser.find_element_by_id('body')
+        page_text = self.browser.find_element_by_id('body').text
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertIn('우유 사기', page_text)
 
