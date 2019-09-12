e38c70c7c12aa1a1d7878c9acb47e9f9c27250f9
hj <hj@toss.im>
2019-09-12 21:54:43 +0900

    time.sleep을 사용한 explict 한 방법을 버린다.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,10 +1,15 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from selenium.common.exceptions import WebDriverException
+
+import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -12,10 +17,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
@@ -38,12 +51,7 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: 공작깃털 사기" 아이템이 추가된다.
         inputbox.send_keys(Keys.ENTER)
 
-        import time
-        time.sleep(2)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.check_for_row_in_list_table('1: 공작깃털 사기')
+        self.wait_for_row_in_list_table('1: 공작깃털 사기')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다.
@@ -51,12 +59,9 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
         inputbox.send_keys(Keys.ENTER)
 
-        time.sleep(2)
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.check_for_row_in_list_table('1: 공작깃털 사기')
-        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.wait_for_row_in_list_table('1: 공작깃털 사기')
+        self.wait_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
 
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다.
         self.fail('Finish the test!')
