0577ff7490285e555a6ae6742390ceacad00623d
hj <hj@toss.im>
2019-09-13 22:40:11 +0900

    * List 모델 추가 * 아이템 생성시 리스트를 항상 생성해서 같이 묶는다. * 버그이긴 한데, 아이템을 입력하면 항상 새로운 리스트가 생기고 화면에는 리스트에 상관 없이 모든 아이템이 한 번에 표시된다.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,10 +1,6 @@
 from django.test import TestCase
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 
-from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -16,15 +12,23 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = '첫 번째 아이템'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = '두 번째 아이템'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(list_, saved_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -32,7 +36,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "첫 번째 아이템")
+        self.assertEqual(first_saved_item.list, saved_list)
         self.assertEqual(second_saved_item.text, "두 번째 아이템")
+        self.assertEqual(second_saved_item.list, saved_list)
 
 
 class ListViewTest(TestCase):
@@ -41,8 +47,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='신규 작업 아이템 1')
-        Item.objects.create(text='신규 작업 아이템 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='신규 작업 아이템 1', list=list_)
+        Item.objects.create(text='신규 작업 아이템 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
