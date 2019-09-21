b6b77be0ef9dd9ca2498f9fc5913ad8b24fb2a56
Hojoon Ji <sleeperbus@gmail.com>
2019-09-21 23:17:31 +0900

    production 설정을 위해 환경 변수를 사용한다.

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9k%pk6sgjtiy^_j4-tax0968i1wfg(#s4l&zn+^4)mw+)mhgyj'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG=False
+    SECRET_KEY=os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS=[os.environ['SITENAME']]
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = ['*']
+    SECRET_KEY = 'insecure-key-for-dev'
 
 
 # Application definition
