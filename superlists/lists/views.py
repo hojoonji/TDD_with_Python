44b310719e82f06f654737b83616e11e164fcae0
Hojoon Ji <sleeperbus@gmail.com>
2019-11-03 22:37:47 +0900

    Refactor list view to handle new item POSTs

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -29,7 +29,3 @@ def new_list(request):
 
     return redirect(f'/lists/{list_.id}/')
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST.get('item_text', ''), list=list_)
-    return redirect(f'/lists/{list_.id}/')
