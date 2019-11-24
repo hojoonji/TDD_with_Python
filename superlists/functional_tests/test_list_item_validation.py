1b2bf75057b245b06a09acbcbaa4ca127f6b756c
Hojoon Ji <sleeperbus@gmail.com>
2019-11-24 22:05:41 +0900

    * use form in all views, back to working state

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -13,14 +13,12 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # 홈페이지는 리프레쉬 됐고 빈 아이템은 입력할 수 없다는 에러가 떴다.
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "빈 아이템을 입력할 수 없습니다"
-            ) 
-        )
+        # 브라우저가 request를 인터셉트하고 리스트 페이지가 읽어들이지 않는다.
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:invalid'))
 
         # 그녀는 이번에는 제대로 된 텍스트를 입력했고 제대로 작동했다. 
         self.get_item_input_box().send_keys('우유 사기')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:valid'))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: 우유 사기')
 
@@ -28,11 +26,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # 그녀는 다시 비슷한 에러를 받았다.
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "빈 아이템을 입력할 수 없습니다"
-            ) 
-        )
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:invalid'))
 
         # 그년 이번에는 제대로 입력하기로 했다.
         self.get_item_input_box().send_keys('차 만들기')
