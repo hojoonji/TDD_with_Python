c640f4bb4b823bd379d455c91a2cf8d63df453e0
Hojoon Ji <sleeperbus@gmail.com>
2019-12-17 23:55:36 +0900

    * send_email, mocking

---
+++ superlists/accounts/tests/test_views.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+
+import accounts.views
+
+class SendLoginEmailViewTeest(TestCase):
+
+    def test_redirects_home_page(self):
+        response = self.client.post('/accounts/send_login_email', data={'email': 'hj@toss.im'})
+        self.assertRedirects(response, '/')
+
+    def test_sends_mail_to_address_from_post(self):
+        self.send_mail_called = False
+
+        def fake_send_mail(subject, body, from_email, to_list):
+            self.send_mail_called = True
+            self.subject = subject
+            self.body = body
+            self.from_email = from_email
+            self.to_list = to_list
+
+        accounts.views.send_mail = fake_send_mail
+        
+        self.client.post('/accounts/send_login_email', data={'email': 'hj@toss.im'})
+        
+        self.assertTrue(self.send_mail_called)
+        self.assertEqual(self.subject, 'Your login link for Superlists')
+        self.assertEqual(self.from_email, 'noreply@superlists')
+        self.assertEqual(self.to_list, ['hj@toss.im'])
+
