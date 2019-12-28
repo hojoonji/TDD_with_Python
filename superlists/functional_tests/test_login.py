03ea25790db2dc53aefdf66ded862b3e00ab0467
hj <hj@toss.im>
2019-12-28 18:59:01 +0900

    * pre created session, log in check 로직을 base로 옮김

--- superlists/functional_tests/test_login.py
+++ superlists/functional_tests/test_login.py
@@ -35,20 +35,13 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
         
         # 그녀는 로그인에 성공했다.
-        self.wait_for(lambda: self.browser.find_element_by_link_text('Log out'))
-        
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # 이제 로그아웃 한다.
         self.browser.find_element_by_link_text('Log out').click()
 
         # 로그 아웃 됐다.
-        self.wait_for(
-            lambda : self.browser.find_element_by_name('email')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(TEST_EMAIL, navbar.text)
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
 
 
